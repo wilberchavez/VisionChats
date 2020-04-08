6977caef2f1a8fb2d27a092c1d441052c7ada914
Rpyung23 <guaman1579@gmail.com>
2020-04-08 17:27:16 -0500

    commit patricio2

--- app/src/main/java/com/visionchatsvirtualcode7ec/visionchats/MainActivity.java
+++ app/src/main/java/com/visionchatsvirtualcode7ec/visionchats/MainActivity.java
@@ -12,21 +12,19 @@ import com.visionchatsvirtualcode7ec.visionchats.Views.registroActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-    ImageButton btn_inicio_session_java;
-    ImageButton btn_registro_java;
+    Button btn_inicio_session_java;
+    Button btn_registro_java;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_inicio_session_java=findViewById(R.id.btn_iniciar_session);
-        btn_registro_java=findViewById(R.id.btn_registro);
+        btn_registro_java = findViewById(R.id.btn_registro_xml);
         btn_registro_java.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, registroActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,registroActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/visionchatsvirtualcode7ec/visionchats/Views/registroActivity.java
+++ app/src/main/java/com/visionchatsvirtualcode7ec/visionchats/Views/registroActivity.java
@@ -2,6 +2,7 @@ package com.visionchatsvirtualcode7ec.visionchats.Views;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -11,68 +12,99 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import com.visionchatsvirtualcode7ec.visionchats.R;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class registroActivity extends AppCompatActivity
 {
+    Toolbar toolbar_java;
     private static final int SELECT_FILE = 1;
     ImageView imageView_photo_perfil_java;
-    Uri selectedImageUri = null;
-    Uri selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        imageView_photo_perfil_java=findViewById(R.id.img_photo_perfil_xml);
+        toolbar_java=findViewById(R.id.toolbar_xml_resgistro_);
+        imageView_photo_perfil_java = findViewById(R.id.perfil_imagen_xml);
+        setSupportActionBar(toolbar_java);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         imageView_photo_perfil_java.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                photo_recoger();
+            public void onClick(View v)
+            {
+                abrir_galeria();
             }
         });
     }
 
-    private void photo_recoger()
+    private void abrir_galeria()
     {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        gallery.setType("image/*");
-        startActivityForResult(Intent.createChooser(gallery, "Seleccione una imagen"),SELECT_FILE);
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Seleccione una imagen"),
+                SELECT_FILE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
     {
-        super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode)
         {
             case SELECT_FILE:
-                if (resultCode == Activity.RESULT_OK) {
-                    selectedImage = data.getData();
-                    String selectedPath=selectedImage.getPath();
-                    if (requestCode == SELECT_FILE) {
-                        if (selectedPath != null) {
-                            InputStream imageStream = null;
-                            try {
-                                imageStream = getContentResolver().openInputStream(selectedImage);
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                            }
-                            // Transformamos la URI de la imagen a inputStream y este a un Bitmap
-                            Bitmap bmp = BitmapFactory.decodeStream(imageStream);
-                            // Ponemos nuestro bitmap en un ImageView que tengamos en la vista
-                            imageView_photo_perfil_java.setImageBitmap(bmp);
-                        }
-                    }
+                if (resultCode==RESULT_OK)
+                {
+                    Uri uri = data.getData();
+                    croopfoto(uri);
+                    imageView_photo_perfil_java.setImageURI(uri);
+                }
+                break;
+            case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE:
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
+                    Uri resultUri = result.getUri();
+                    crearbitmapphoto(resultUri);
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Exception error = result.getError();
                 }
                 break;
         }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void crearbitmapphoto(Uri resultUri)
+    {
+        String selectedPath=resultUri.getPath();
+            if (selectedPath != null) {
+                InputStream imageStream = null;
+                try {
+                    imageStream = getContentResolver().openInputStream(
+                            resultUri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                // Transformamos la URI de la imagen a inputStream y este a un Bitmap
+                Bitmap bmp = BitmapFactory.decodeStream(imageStream);
+                // Ponemos nuestro bitmap en un ImageView que tengamos en la vista
+                imageView_photo_perfil_java.setImageBitmap(bmp);
+            }
     }
 
 
+    private void croopfoto(Uri uri)
+    {
+        CropImage.activity(uri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+    }
 }
